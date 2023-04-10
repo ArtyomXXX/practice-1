cbb76f676e623e9ec2e213f97ad135b03d5d76ec
ArtyomXXX <ambartsumyanartyomxxx@gmail.com>
2023-04-11 02:06:54 +0300

    .commit.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
-
+const modal = document.getElementById('modal');
+const cancelBtn = document.getElementById('cancel-button');
+const openModalBtn = document.getElementById('open-modal-button');
 
 cancelBtn.addEventListener("click", () => {
 	modal.style.display = "none";
